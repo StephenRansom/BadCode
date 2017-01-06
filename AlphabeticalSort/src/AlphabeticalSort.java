bd74f6fb4f8e85d8d73497ef63d15e7d3bf63be9
StephenRansom <stpransom@gmail.com>
2017-01-05 21:14:36 -0700

    The code that inspired this repository

---
+++ AlphabeticalSort/src/AlphabeticalSort.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+/**
+ * The code that inspired it all
+ */
+public class AlphabeticalSort {
+    public static void main(String args[]){
+        String s = "typewriter";
+
+        for(int i = 0; i < Integer.MAX_VALUE; i++){
+            s = randomSort(s);
+
+            if(s.equals("eeiprrttwy")){
+                System.out.println(s);
+                break;
+            }
+
+            if(i == Integer.MAX_VALUE-1){
+                i=0;
+            }
+        }
+    }
+
+
+
+    private static String randomSort(String s){
+        char[] temp = s.toCharArray();
+        Random r = new Random();
+
+        //sort array
+        for(int i = 0; i < 320489; i+=2){
+            int first  = r.nextInt(10);
+            int second = r.nextInt(10);
+            System.out.println(first);
+            char t1 = temp[first];
+            temp[first]=temp[second];
+            temp[second]=t1;
+        }
+
+        return s;
+    }
+
+
+
+}
