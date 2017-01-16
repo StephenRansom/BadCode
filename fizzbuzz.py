6f5e5e9b4a8d3a52f1727a298eeba7a2f2b98435
Tristan Garwood <tristan@ghostinajar.io>
2017-01-16 17:56:24 -0500

    Update fizzbuzz.py
    Made it bad-er

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,22 +3,22 @@ b=5
 l = 1
 I = 1
 i = 1
-while True:
+while 1:
     fb=False
-    while True:
+    while 1:
         if i==f*l:
             print 'fizz'
             fb=True
             l+=1
             i+=1
             break
-        elif i==b*I:
+        if i==b*I:
             print 'buzz'
             fb=True
             I+=1
             i+=1
             break
-        else:
+        if 1:
             print i
             i+=1
             break
