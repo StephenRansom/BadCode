0f09ca89d489e7bffafaa4c20260d2ad35591525
StephenRansom <stephen.ransom@pm.me>
2020-06-20 18:00:43 -0600

    true or false

---
+++ trueOrFalse.js
@@ -0,0 +1,12 @@
+/**
+ * usage
+ * if (!trueOrFalse(x != y)) ...
+ */
+boolean trueOrFalse(boolean b){
+    if(b == true){
+        return true;
+    } else if(x == false){
+        return false;
+    }
+    return false;
+}
